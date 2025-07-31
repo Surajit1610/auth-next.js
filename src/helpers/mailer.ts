005aeb4cc3aa2aee3e5f942b34866d416ed71658
Surajit Mondal <surajitmondal2130@gmail.com>
2025-07-31 20:22:36 +0530

    second commit

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -14,7 +14,7 @@ export const sendEmail = async({email, emailType, userId}: any)=> {
                 {forgotPasswordToken:hashedToken, forgotPasswordTokenExpiry: Date.now()+ 3600000})
         }
 
-    var transport = nodemailer.createTransport({
+    const transport = nodemailer.createTransport({
       host: "sandbox.smtp.mailtrap.io",
       port: 2525,
       auth: {
